a54131e3d89ed24ce56652d2c899b6e9b962c0ab
Victor Vinicius <victorsomulex@gmail.com>
2023-10-12 14:41:25 -0300

    criando component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <TesteComponent />
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TesteComponent from './components/TesteComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TesteComponent
   }
 }
 </script>
---
+++ src/components/TesteComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <h1 class="header">
+            Componente Header
+        </h1>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header {
+    background-color: rgba(0, 0, 0, 0.5);
+    font-size:30px;
+    color: black;
+    padding-top: 10px;
+}
+</style>
